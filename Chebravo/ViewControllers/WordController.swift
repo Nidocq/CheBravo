f243259623c6106b548509c59a25fd935cf618b5
Nidocq <phillip.linnemann.lundin@gmail.com>
2022-07-01 21:46:36 +0200

    Add documentation

--- Chebravo/ViewControllers/WordController.swift
+++ Chebravo/ViewControllers/WordController.swift
@@ -9,12 +9,14 @@ import Foundation
 import SwiftUI
 import CoreData
 
-
+/// <summary> Controls the behaviour of the words with the help of viewContexts </summary>
+/// <remarks> I can't instantiate the NSManagedObjectContext with this class for some reason, even though
+/// it probably would b better </remarks>
 class WordController : ObservableObject {
     
-    
-    ///
-    /// <arg="viewContext"> </arg>
+    /// <summary> Creates and adds a specified item to the NSManagedObjectContext. </summary>
+    /// <param name="viewContext"> The NSManagedObjectContext given in which the item needs to be saved to</param>
+    /// <param name="WordName"> The name of the word that needs to be saved to the viewContext</param>
     func addItem(viewContext : NSManagedObjectContext, Wordname : String) {
         guard (Wordname != "") else { return }
         withAnimation {
@@ -24,6 +26,10 @@ class WordController : ObservableObject {
         }
     }
     
+    
+    /// <summary> Removes an item from the NSManagedObjectContext </summary>
+    /// <param name="viewContext"> NSManagedObjectContext in which the word needs to be removed </param>
+    /// <param name="word"> Word class that needs to be deleted from the viewContext </param>
     func removeItem(viewContext : NSManagedObjectContext, word : Word) {
         withAnimation {
             viewContext.delete(word)
