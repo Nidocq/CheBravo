0c8893d56cea193006ed125557f556dc78c1441d
Nidocq <phillip.linnemann.lundin@gmail.com>
2022-07-24 11:38:15 +0200

    MVP of Learning numbers

---
+++ Chebravo/Views/Learn/ILearning.swift
@@ -0,0 +1,13 @@
+//
+//  ILearning.swift
+//  Chebravo
+//
+//  Created by Phillip Lundin on 23/07/2022.
+//
+
+import Foundation
+
+
+protocol ILearning {
+    
+}
---
+++ Chebravo/Views/Learn/LearnNumbers.swift
@@ -0,0 +1,20 @@
+//
+//  LearnNumbers.swift
+//  Chebravo
+//
+//  Created by Phillip Lundin on 23/07/2022.
+//
+
+import SwiftUI
+
+struct LearnNumbers: View, ILearning {
+    var body: some View {
+        Text("hello")
+    }
+}
+
+struct LearnNumbers_Previews: PreviewProvider {
+    static var previews: some View {
+        LearnNumbers()
+    }
+}
