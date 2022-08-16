b3d8515c3a51aeafd2a4c657e7bb87b3a14c8bef
Nidocq <phillip.linnemann.lundin@gmail.com>
2022-08-16 23:31:00 +0200

    Add textInput with responsiveness

--- Chebravo/Views/ExampleView.swift
+++ Chebravo/Views/ExampleView.swift
@@ -20,16 +20,20 @@ struct ExampleView: View {
             
             VStack {
                 TextField(
+                    // TODO: Make the text be there with userDefaults, even though the user exists the view to save progress
                     "Type in an example that should be translated",
                     text: $textInput
                 )
+                .frame(height: 50)
                 .focused($emailFieldIsFocused)
                 .textInputAutocapitalization(.never)
                 .disableAutocorrection(true)
                 .border(.secondary)
+                .padding()
                 
                 Text(textInput)
                         .foregroundColor(emailFieldIsFocused ? .red : .blue)
+                        .font(.system(size: 24))
             }
 
         }
