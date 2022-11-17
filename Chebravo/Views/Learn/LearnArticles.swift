991c2595c263e7e2ef2fda50a846db6904822c1c
Nidocq <phillip.linnemann.lundin@gmail.com>
2022-11-17 13:51:38 +0100

    Update to Articles

--- Chebravo/Views/Learn/LearnArticles.swift
+++ Chebravo/Views/Learn/LearnArticles.swift
@@ -14,12 +14,11 @@ struct ModuleArticles : ILearning {
     static let MATERIAL_TITLE : String = "Articles"
     static let ICON_NAME : String = "textformat.subscript"
     let paddingLeftRight : CGFloat = 14
+    let titleSize : CGFloat = 22
 
     var body: some View {
         VStack(alignment: .leading) {
-            Text("Articles clarifies the condition of a noun, like how many there are, or if you refer to a specific thing or not.")
-            Text("English has only 2 articles: **\"the\"** and **'a'**")
-            Text("But Italian has a 7 articles! They are listed below on how they are used")
+            Text("Articles clarifies the condition of a noun, like how many there are, or if you refer to a specific thing or not. English has only 2 articles: **\"the\"** and **'a'**, but Italian has a 7 articles! They are listed below on how they are used")
                 
         }
             .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .leading)
@@ -29,53 +28,121 @@ struct ModuleArticles : ILearning {
         
         VStack(spacing:0) {
             Seperator()
+                .padding(.bottom, 15)
             VStack {
                 Text("♀")
                 Text("Feminine articles")
-                    .font(.system(size: 18))
+                    .font(.system(size: titleSize))
+                    .bold()
+                VStack {
+                    Text("Article \"la\" is used for every feminine article, unless there is a vovel then we use \"l\'\". If you talk about several nouns, \"le\" is the article to describe the plural")
+                        .font(.system(size: 18))
+                }
+                .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .leading)
+            }
+                .padding(.bottom, 20)
+                .font(.system(size: 40))
+            
+            // Individual infographics for the Articles
+            VStack {
+                Text("Definite")
+                    .italic()
+                    .font(.system(size: 16))
+                    .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .leading)
+                ArticleType (
+                    Article: "la",
+                    SingularOrPlural: SuperscriptSingular as! AnyView,
+                    ExampleIT: "la mattina",
+                    ExampleEN: "The morning"
+                )
+                
+                ArticleType(
+                    Article: "l\'",
+                    SingularOrPlural: SuperscriptSingular as! AnyView,
+                    ExampleIT: "l\'isola",
+                    ExampleEN: "The island"
+                )
+                
+                ArticleType(
+                    Article: "le",
+                    SingularOrPlural: SuperscriptPlural as! AnyView,
+                    ExampleIT: "le mattine",
+                    ExampleEN: "The mornings"
+                    // TODO: Optional argument paramaeter for le isole
+                )
+                
+                Button {
+                    //code
+                } label: {
+                    Text("Show indefine articles")
+                        .foregroundColor(Color("PrimaryColor"))
+                        .bold()
+                        .padding(.top, 16)
+                        .frame(width: 230, height: 50)
+                        .background(Color("SecondaryColor"))
+                        .cornerRadius(11)
+                }
+                //TODO: Make text in the middle of button
+                .padding(.top, 20)
+
                 
             }
-                .padding()
+                .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .leading)
+        }
+        .foregroundColor(Color("SecondaryColor"))
+        .padding([.leading, .trailing], paddingLeftRight)
+        // TODO: Make toggle button for the indefinite (uno una un etc) articles
+        // removing the plural and chaning la and l' to una un' as well as the examples.
+        
+        VStack(spacing:0) {
+            Seperator()
+                .padding(.bottom, 20)
+                .padding(.top, 40)
+            VStack {
+                Text("♂")
+                Text("Masculine articles")
+                    .font(.system(size: titleSize))
+                    .bold()
+                VStack {
+                    Text("lo and")
+                        .font(.system(size: 18))
+                }
+                .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .leading)
+                
+            }
+                .padding(.bottom, 20)
                 .font(.system(size: 40))
             
             // Individual infographics for the Articles
             VStack {
                 ArticleType (
-                    Article: "la",
+                    Article: "lo",
                     SingularOrPlural: SuperscriptSingular as! AnyView,
-                    ExampleIT: "la matita",
+                    ExampleIT: "la mattina",
                     ExampleEN: "The morning"
                 )
                 
-                HStack {
-                Text("**l\'**")
-                    .underline()
-                    .font(.system(size: 22))
-                    SuperscriptSingular
-                }
-                    .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
-                //LearningExample(ItalianExample: "l\'isola", EnglishTranslation: "The island")
+                ArticleType(
+                    Article: "gli",
+                    SingularOrPlural: SuperscriptSingular as! AnyView,
+                    ExampleIT: "l\'isola",
+                    ExampleEN: "The island"
+                )
                 
-                VStack {
-                    HStack {
-                        Text("**le**")
-                            .underline()
-                            .font(.system(size: 22))
-                        SuperscriptPlural
-                    }
-                    LearningExample(ItalianExample: "le matine", EnglishTranslation: "The mornings")
-                        .frame(maxWidth: .infinity, maxHeight: 40)
-                    LearningExample(ItalianExample: "l'isole", EnglishTranslation: "The islands")
-                        .frame(maxWidth: .infinity, maxHeight: 40)
-                    
-                }
-                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                ArticleType(
+                    Article: "le",
+                    SingularOrPlural: SuperscriptPlural as! AnyView,
+                    ExampleIT: "le mattine",
+                    ExampleEN: "The mornings"
+                    // TODO: Optional argument paramaeter for le isole
+                )
                 
             }
-                    .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .leading)
+                .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .leading)
         }
         .foregroundColor(Color("SecondaryColor"))
         .padding([.leading, .trailing], paddingLeftRight)
+        
     }
 }
 
@@ -95,6 +162,7 @@ struct ArticleType: View {
     var SingularOrPlural : AnyView
     var ExampleIT : String
     var ExampleEN : String
+    let paddingAllAround : CGFloat = 14
     var body: some View {
         VStack(spacing: 0) {
             HStack {
@@ -105,7 +173,7 @@ struct ArticleType: View {
                 SingularOrPlural
             }
             .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
-            .padding()
+            .padding(paddingAllAround)
             .background(Color("SecondaryColor"))
             LearningExample(
                 ItalianExample: ExampleIT,
