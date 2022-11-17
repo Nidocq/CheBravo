5262ef04e35d9cf8cb23e86a0d864b98c0ae2183
Nidocq <phillip.linnemann.lundin@gmail.com>
2022-11-17 11:10:55 +0100

    Add update for pronunciation

--- Chebravo/Views/Learn/LearnPronunciation.swift
+++ Chebravo/Views/Learn/LearnPronunciation.swift
@@ -17,58 +17,131 @@ struct ModulePronunciation: ILearning {
     
     var body: some View {
         VStack(spacing: 24) {
-            Text("Italians use 'c' extensively, so when it comes to pronunciation is vital to pronunce it right.")
-            
-            // Pronunciation graphics together
-            HStack(spacing: 30) {
-                PronunciationGraphic(
-                    pronouncingWord: "c",
-                    firstVowel: "a",
-                    secondVowel: "u",
-                    thirdVowel: "o"
-                )
-                Text("and")
-                    .padding(.trailing, -10)
-                PronunciationGraphic(
-                    pronouncingWord: "ch",
-                    firstVowel: "i",
-                    secondVowel: "e"
-                )
-            }
-            
-            VStack {
-                Image(systemName: "arrow.down")
-                    .font(.system(size: 38))
-                    .padding(.bottom, 20)
-                
-                
-                WordPronunciation(
-                    pronunciation: "k [k]",
-                    audioName: "k.mp3",
-                    audioController: AC
-                )
-                
-                
-            }
-            
             VStack(spacing: 0) {
-                LearningExample(
-                    ItalianExample: "Noi andiamo ancora? [anˈko.ra]",
-                    EnglishTranslation: "Are we going again?"
-                )
-                .padding(.bottom)
-                LearningExample(
-                    ItalianExample: "Noi andiamo ancora? [anˈko.ra]",
-                    EnglishTranslation: "Are we going again?"
-                )
-                .padding(.bottom)
-                LearningExample(
-                    ItalianExample: "Noi andiamo ancora? [anˈko.ra]",
-                    EnglishTranslation: "Are we going again?"
-                )
+            Text("Italians use 'c' extensively, so when it comes to pronunciation is vital to pronunce it right. This module includes IPA pronunciation techniques. We advice to read more on IPA")
+                    .padding(.bottom)
+                Button {
+                    if let url = URL(string: "https://en.wikipedia.org/wiki/International_Phonetic_Alphabet") {
+                        UIApplication.shared.open(url)
+                    }
+                } label: {
+                    Text("IPA")
+                        .foregroundColor(.blue)
+                        .frame(width: 70, height: 30)
+                        .background(Color("SecondaryColor"))
+                        .cornerRadius(10)
+                }
             }
-            .padding([.leading, .trailing])
-            .padding([.top, .bottom], 16)
+            
+            // Pronunciation graphics together c and ch makes k
+            VStack {
+                HStack(spacing: 30) {
+                    PronunciationGraphic(
+                        pronouncingWord: "c",
+                        firstVowel: "a",
+                        secondVowel: "u",
+                        thirdVowel: "o"
+                    )
+                    Text("and")
+                        .padding(.trailing, -10)
+                    PronunciationGraphic(
+                        pronouncingWord: "ch",
+                        firstVowel: "i",
+                        secondVowel: "e"
+                    )
+                }
+                
+                VStack {
+                    Image(systemName: "arrow.down")
+                        .font(.system(size: 38))
+                        .padding(.bottom, 20)
+                    
+                    
+                    WordPronunciation(
+                        pronunciation: "k [k]",
+                        audioName: "k.mp3",
+                        audioController: AC
+                    )
+                    
+                    
+                }
+                
+                VStack(spacing: 0) {
+                    LearningExample(
+                        ItalianExample: "Vuoi una caramella? [kaɾamˈɛlla]",
+                        EnglishTranslation: "Do you want a candy?"
+                    )
+                    .padding(.bottom)
+                    LearningExample(
+                        ItalianExample: "Che [k'e] cosa [kˈɔza] stai facendo?",
+                        EnglishTranslation: "What are you doing?"
+                    )
+                    .padding(.bottom)
+                    LearningExample(
+                        ItalianExample: "Chi [ki] è che cucina? [kʊt͡ʃˈina]",
+                        EnglishTranslation: "Who's cooking?"
+                    )
+                }
+                .padding([.leading, .trailing])
+                .padding(.top, 16)
+                .padding(.bottom, 50)
+            }
+            
+            Seperator()
+            
+            // Pronunciation graphics together c and ci makes ci
+            VStack {
+                HStack(spacing: 30) {
+                    PronunciationGraphic(
+                        pronouncingWord: "c",
+                        firstVowel: "i",
+                        secondVowel: "e"
+                    )
+                    Text("and")
+                        .padding(.trailing, -10)
+                    PronunciationGraphic(
+                        pronouncingWord: "ci",
+                        firstVowel: "a",
+                        secondVowel: "u",
+                        thirdVowel: "o"
+                    )
+                }
+                
+                VStack {
+                    Image(systemName: "arrow.down")
+                        .font(.system(size: 38))
+                        .padding(.bottom, 20)
+                    
+                    WordPronunciation(
+                        pronunciation: "tch [tʃ]",
+                        audioName: "tch.mp3",
+                        audioController: AC
+                    )
+                }
+                
+                VStack(spacing: 0) {
+                    LearningExample(
+                        ItalianExample: "Noi andiamo ancora? [anˈko.ra]",
+                        EnglishTranslation: "Are we going again?"
+                    )
+                    .padding(.bottom)
+                    LearningExample(
+                        ItalianExample: "Noi andiamo ancora? [anˈko.ra]",
+                        EnglishTranslation: "Are we going again?"
+                    )
+                    .padding(.bottom)
+                    LearningExample(
+                        ItalianExample: "Noi andiamo ancora? [anˈko.ra]",
+                        EnglishTranslation: "Are we going again?"
+                    )
+                }
+                .padding([.leading, .trailing])
+                .padding(.top, 16)
+                .padding(.bottom, 50)
+            }
+            
+            
+            
         }
         .foregroundColor(Color("SecondaryColor"))
     }
